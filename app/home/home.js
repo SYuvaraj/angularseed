12a07b656c581d5d940ab309a698537200e645d1
Yuvaraj <yuvaraj@mantralabsglobal.com>
2016-03-29 00:14:48 +0530

    ladda loading removed

--- app/home/home.js
+++ app/home/home.js
@@ -84,23 +84,23 @@ angular.module('myApp.home', ['ngRoute','firebase'])
 	};
     
 }])
-.directive('laddaLoading', [
-    function() {
-        return {
-            link: function(scope, element, attrs) {
-                var Ladda = window.Ladda;
-                var ladda = Ladda.create(element[0]);
-                //watch login.loading for change
-                scope.$watch(attrs.laddaloading, function(newVal, oldVal){
-                    //Based on the value start and stop the indicator
-                    if(newVal){
-                        ladda.start();
-                    }
-                    else{
-                        ladda.stop();
-                    }
-                });
-            }
-        };
-    }
-]);
+// .directive('laddaLoading', [
+//     function() {
+//         return {
+//             link: function(scope, element, attrs) {
+//                 var Ladda = window.Ladda;
+//                 var ladda = Ladda.create(element[0]);
+//                 //watch login.loading for change
+//                 scope.$watch(attrs.laddaloading, function(newVal, oldVal){
+//                     //Based on the value start and stop the indicator
+//                     if(newVal){
+//                         ladda.start();
+//                     }
+//                     else{
+//                         ladda.stop();
+//                     }
+//                 });
+//             }
+//         };
+//     }
+// ]);
